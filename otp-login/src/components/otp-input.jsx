e7bcc4bbbdf1c3abcea3f451daed6a0da559e9f2
vivek koyani <vivek.koyani@gmail.com>
2024-04-06 00:43:15 +0530

    add login otp functionality

---
+++ otp-login/src/components/otp-input.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+
+const OtpInput = ({ length = 4, onOtpSubmit = () => {} }) => {
+  const [otp, setOtp] = useState(new Array(length).fill(""));
+  const inputRefs = useRef([]);
+
+  useEffect(() => {
+    if (inputRefs.current[0]) {
+      inputRefs.current[0].focus();
+    }
+  }, []);
+
+  const handleChange = (index, e) => {
+    const value = e.target.value;
+    if (isNaN(value)) return;
+
+    const newOtp = [...otp];
+
+    // allow only one input
+    newOtp[index] = value.substring(value.length - 1);
+    setOtp(newOtp);
+
+    // submit trigger
+    const combinedOtp = newOtp.join("");
+    if (combinedOtp.length === length) onOtpSubmit(combinedOtp);
+
+    // Move to next input if current field is filed
+    if(value && index < length - 1 && inputRefs.current[index + 1]) {
+        inputRefs.current[index + 1].focus();
+    }
+  };
+  const handleClick = () => {};
+
+  return (
+    <div>
+      {otp.map((value, index) => {
+        return (
+          <input
+            key={index}
+            type="text"
+            ref={(input) => (inputRefs.current[index] = input)}
+            value={value}
+            onChange={(e) => handleChange(index, e)}
+            onClick={() => handleClick(index)}
+            onKeyDown={() => handleKeyDown(index, e)}
+            className="otpInput"
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default OtpInput;
