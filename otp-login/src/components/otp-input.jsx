fdcda8fe2eb88bcfd1bfbfc977cedd53912a9c27
vivek koyani <vivek.koyani@gmail.com>
2024-04-13 19:44:05 +0530

    changes add for go to previous empty input

--- otp-login/src/components/otp-input.jsx
+++ otp-login/src/components/otp-input.jsx
@@ -25,11 +25,30 @@ const OtpInput = ({ length = 4, onOtpSubmit = () => {} }) => {
     if (combinedOtp.length === length) onOtpSubmit(combinedOtp);
 
     // Move to next input if current field is filed
-    if(value && index < length - 1 && inputRefs.current[index + 1]) {
-        inputRefs.current[index + 1].focus();
+    if (value && index < length - 1 && inputRefs.current[index + 1]) {
+      inputRefs.current[index + 1].focus();
     }
   };
-  const handleClick = () => {};
+  const handleClick = (index) => {
+    inputRefs.current[index].setSelectionRange(1, 1);
+
+    // optional
+    if(index > 0 && !otp[index - 1]){
+      inputRefs.current[otp.indexOf("")].focus();
+    }
+  };
+
+  const handleKeyDown = (index, e) => {
+    if (
+      e.key === "Backspace" &&
+      !otp[index] &&
+      index > 0 &&
+      inputRefs.current[index - 1]
+    ) {
+      // Move focus to the previous input field on backspace
+      inputRefs.current[index - 1].focus();
+    }
+  };
 
   return (
     <div>
@@ -42,7 +61,7 @@ const OtpInput = ({ length = 4, onOtpSubmit = () => {} }) => {
             value={value}
             onChange={(e) => handleChange(index, e)}
             onClick={() => handleClick(index)}
-            onKeyDown={() => handleKeyDown(index, e)}
+            onKeyDown={(e) => handleKeyDown(index, e)}
             className="otpInput"
           />
         );
